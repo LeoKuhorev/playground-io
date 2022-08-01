b9c91119cb8f30e4cacdb3d1ed6c67970375bf36
Leo Kukharau <leo.kukharau@microsoft.com>
2022-08-01 11:03:30 -0700

    Add demo flow

--- io_utils.py
+++ io_utils.py
@@ -1,5 +1,4 @@
 import curses
-from logging import ERROR
 import re
 
 from exceptions import QuitException
@@ -8,83 +7,101 @@ from exceptions import QuitException
 class IO:
     VALID_EMAIL = r"[^@]+@[^@]+\.[^@]+"
     VALID_PHONE = r"^\d{3}-\d{3}-\d{4}$"
+    VALID_PRICE = r"^\d{1,3}(,\d{3})*(\.\d{2})?$"
+    VALID_URL = r"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$"
     ANY = r".*"
 
-    STYLE_DEFAULT = 1
-    STYLE_HIGHLIGHT = 2
-    STYLE_ERROR = 3
+    COLOR_DEFAULT = 1
+    COLOR_WARNING = 2
+    COLOR_ERROR = 3
+    COLOR_SUCCESS = 4
+    COLOR_REVERSE = 5
 
     def __init__(self):
         self.text = ''
-        self.options: list[str] = []
         self.selection = ''
 
-    def __render_options(self, stdscr: any, selected_row_idx: int) -> None:
+    def __init_color_scheme(self) -> None:
+        curses.curs_set(0)
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
+        curses.init_pair(4, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        curses.init_pair(5, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+    def __render_options(self, stdscr: any, selected_row_idx: int, options: list[str], style: int = COLOR_REVERSE) -> None:
         stdscr.clear()
         h, w = stdscr.getmaxyx()
 
-        for idx, row in enumerate(self.options):
-            x = w // 2 - len(row) // 2
+        for idx, row in enumerate(options):
+            x = max(w // 2 - len(row) // 2, 0)
             y = h // 2 + idx
 
             if idx == selected_row_idx:
-                stdscr.attron(curses.color_pair(1))
+                stdscr.attron(curses.color_pair(style))
                 stdscr.addstr(y, x, row)
-                stdscr.attroff(curses.color_pair(1))
+                stdscr.attroff(curses.color_pair(style))
             else:
                 stdscr.addstr(y, x, row)
 
         stdscr.refresh()
 
-    def __handle_option_selection(self, stdscr: any) -> None:
-        curses.curs_set(0)
-        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
-        current_row_idx = 0
+    def __handle_option_selection(self, stdscr: any, options: list[str], header: str) -> None:
+        self.__init_color_scheme()
+        first_row_idx = current_row_idx = 1 if header else 0
+        if header:
+            options = [header] + options
 
-        self.__render_options(stdscr, current_row_idx)
+        self.__render_options(stdscr, current_row_idx, options)
 
         while True:
             key = stdscr.getch()
             stdscr.clear()
 
-            if key == curses.KEY_UP and current_row_idx > 0:
+            if key == curses.KEY_UP and current_row_idx > first_row_idx:
                 current_row_idx -= 1
-            elif key == curses.KEY_DOWN and current_row_idx < len(self.options) - 1:
+            elif key == curses.KEY_DOWN and current_row_idx < len(options) - 1:
                 current_row_idx += 1
             elif key == curses.KEY_ENTER or key in [10, 13]:
-                self.selection = self.options[current_row_idx]
+                self.selection = options[current_row_idx]
                 break
 
-            self.__render_options(stdscr, current_row_idx)
+            self.__render_options(stdscr, current_row_idx, options)
 
-    def __render_center_print(self, stdscr: any, style:int) -> None:
-        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
-        curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
-        curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
+    def __render_center_print(self, stdscr: any, style: int) -> None:
+        self.__init_color_scheme()
+        stdscr.clear()
 
-        curses.curs_set(0)
         h, w = stdscr.getmaxyx()
 
-        x = w // 2 - len(self.text) // 2
-        y = h // 2
+        split_text = self.text.split('\n')
 
-        stdscr.clear()
-        stdscr.attron(curses.color_pair(style))
-        stdscr.addstr(y, x, self.text)
-        stdscr.attroff(curses.color_pair(style))
+        for idx, row in enumerate(split_text):
+            x = max(w // 2 - len(row) // 2, 0)
+            y = h // 2 + idx
+
+            stdscr.attron(curses.color_pair(style))
+            stdscr.addstr(y, x, row)
+            stdscr.attroff(curses.color_pair(style))
 
         stdscr.refresh()
         stdscr.getch()
 
-    def __capture_raw_input(self, stdscr: any, r: int, c: int, prompt_string: str) -> str:
+    def __capture_raw_input(self, stdscr: any, prompt_string: str) -> str:
         stdscr.clear()
         curses.echo()
-        stdscr.addstr(r, c, prompt_string)
+
+        h, w = stdscr.getmaxyx()
+
+        x = max(w // 2 - len(prompt_string) // 2, 0)
+        y = h // 2
+
+        stdscr.addstr(y, x, prompt_string)
         stdscr.refresh()
 
-        input = str(stdscr.getstr(r + 1, c, 40))[2:-1]
+        input = str(stdscr.getstr(y + 1, x, 500))[2:-1]
 
-        if input.lower() == 'q' or input.lower() == 'quit':
+        if input.lower().strip() == 'q' or input.lower().strip() == 'quit':
             raise QuitException()
 
         return input
@@ -92,21 +109,21 @@ class IO:
     def __is_valid_input(self, input_string: str, regex: str) -> bool:
         return re.match(regex, input_string) is not None
 
-    def input(self, prompt_string:str, regex: str=ANY) -> str:
-        input = curses.wrapper(self.__capture_raw_input, 0, 0, prompt_string)
+    def input(self, prompt_string: str, regex: str = ANY) -> str:
+        input = curses.wrapper(self.__capture_raw_input, prompt_string)
 
         while not self.__is_valid_input(input, regex):
-            self.print("Invalid input. Please try again.", self.STYLE_ERROR)
-            input = curses.wrapper(self.__capture_raw_input, 0, 0, prompt_string)
+            self.print("Invalid input. Please try again.", self.COLOR_ERROR)
+            input = curses.wrapper(
+                self.__capture_raw_input, prompt_string)
 
         return input
 
-    def print(self, text: str, style:int=STYLE_DEFAULT) -> None:
+    def print(self, text: str, style: int = COLOR_DEFAULT) -> None:
         self.text = text
         curses.wrapper(self.__render_center_print, style)
 
-    def print_options(self, options: list[str]) -> str:
-        self.options = options
-        curses.wrapper(self.__handle_option_selection)
+    def print_options(self, options: list[str], header: str = None) -> str:
+        curses.wrapper(self.__handle_option_selection, options, header)
 
         return self.selection
--- main.py
+++ main.py
@@ -1,40 +1,64 @@
+import random
 from exceptions import QuitException
 from io_utils import IO
+
 
 io = IO()
 
 WELCOME_MESSAGE = "Welcome to our super cool application!!"
 GOODBYE_MESSAGE = "Thank you for using our super cool application. Goodbye!"
+CARRIERS = ["AT&T", "Verizon", "T-Mobile", "Sprint"]
+OPTIONS = ['Add item', 'Edit item', 'Delete item']
+WEBSITES = ["Amazon", "Ebay", "Bestbuy", "Walmart"]
+
+items = []
+
 
 try:
     io.print(WELCOME_MESSAGE)
 
-    # How to capture user input
-    email = io.input('Enter your email:', io.VALID_EMAIL)
-    io.print(f"Your email is: {email}")
+    user_email = io.input('Please enter your email:', io.VALID_EMAIL)
+    io.print("User not found!", style=IO.COLOR_WARNING)
+    phone_number = io.input(
+        'Please enter your phone number in format xxx-xxx-xxxx:', io.VALID_PHONE)
+    user_carrier = io.print_options(CARRIERS, "Please select your carrier:")
 
-    phone_number = io.input('Enter your phone number in format xxx-xxx-xxxx:', io.VALID_PHONE)
-    io.print(f"Your phone number is: {phone_number}")
+    io.print(
+        f"New user created!\nEmail: {user_email}\nPhone: {phone_number}\nCarrier: {user_carrier}", style=IO.COLOR_SUCCESS)
 
-    # How to render multiple options.
-    # Note that the method returns the option that user selected
-    options = ['Add item', 'Edit item', 'Delete item']
-    user_selection = io.print_options(options)
+    while True:
+        user_selection = io.print_options(OPTIONS if len(
+            items) > 0 else OPTIONS[:1], "Please select an option:")
 
-    # So you can build your own logic to handle the user selection
-    if user_selection == options[0]:
-        # do some actual logic here
-        user_input = io.input('Enter item you want to add: ')
-        io.print(f"You have entered {user_input}")
-    elif user_selection == options[1]:
-        # do some actual logic here
-        user_input = io.input('Enter item you want to edit: ')
-        io.print(f"You have entered {user_input}")
-    elif user_selection == options[2]:
-        # do some actual logic here
-        user_input = io.input('Enter item you want to delete: ')
-        io.print(f"You have entered {user_input}")
+        if user_selection == OPTIONS[0]:
+            new_item = {"links": []}
+            new_item['name'] = io.input('Please enter item name: ')
+
+            user_selection = io.print_options(
+                ["Yes", "No"], "Do you want to add a link?")
+            while user_selection == "Yes":
+                website = io.print_options(
+                    WEBSITES, "Please select a website:")
+                link = io.input('Please enter a link:', io.VALID_URL)
+                new_item["links"].append(
+                    {"website": website, "link": link, "price": random.randrange(1, 999)})
+
+                user_selection = io.print_options(
+                    ["Yes", "No"], "Do you want to add a link?")
+
+            new_item["target_price"] = io.input(
+                'Please enter target price:', io.VALID_PRICE)
+            io.print(
+                f"New item created!\n{str(new_item)}", style=IO.COLOR_SUCCESS)
+            items.append(new_item)
+
+        elif user_selection == OPTIONS[1]:
+            selected_item = io.print_options(
+                [str(item) for item in items], "Select an item you want to edit:")
+        elif user_selection == OPTIONS[2]:
+            selected_item = io.print_options(
+                [str(item) for item in items], "Select an item you want to delete:")
 
 except QuitException:
-    # This is made to gracefully handle quite at any moment
+    # This is made to gracefully handle quit at any moment
     io.print(GOODBYE_MESSAGE)
